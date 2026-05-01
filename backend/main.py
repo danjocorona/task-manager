7aceb97f72b38040f5a32b9f15c6cf524eb68fcf
Daniel Corona <daniel.joseph.corona@gmail.com>
2026-05-01 16:45:59 -0700

    Allow Live Server origin in CORS for local development

--- backend/main.py
+++ backend/main.py
@@ -35,7 +35,11 @@ app = FastAPI(
 # to make requests to this API. Restrict origins before deploying.
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://danjocorona.github.io"],
+    allow_origins=[
+        "https://danjocorona.github.io",
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
